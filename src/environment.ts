a42611dea10a8df2d4ce3614367b376b6a1758da
laisefh <laise.fh@aluno.ifsc.edu.br>
2023-05-09 15:15:43 -0300

    arquivos

---
+++ src/environment.ts
@@ -0,0 +1,17 @@
+
+export const environment = {
+
+  production: false,
+
+  firebaseConfig: {
+
+    apiKey: "AIzaSyDP671GcUNv34CDE4hpEse4-VCP8V3mmeQ",
+    authDomain: "crud-angular-8a006.firebaseapp.com",
+    databaseURL: "https://crud-angular-8a006-default-rtdb.firebaseio.com",
+    projectId: "crud-angular-8a006",
+    storageBucket: "crud-angular-8a006.appspot.com",
+    messagingSenderId: "756006154966",
+    appId: "1:756006154966:web:e5c7e98a8bb22e1b5f0f77"
+  }
+
+};
